d545473af9cfe1af8c2adf23acf4d69bccdcf6fe
Joel Seidel <joeldseidel@gmail.com>
2018-12-30 23:57:29 -0500

    Created some scripts

---
+++ Assets/Scripts/Town.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Author: Joel Seidel, Johsua Famous
+ * Controls town dyanmics
+ */
+
+public class Town : MonoBehaviour {
+
+    public string productName;
+    public double elasticity;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
