8c4893b553b2407a621f3192a0677e937f63bfe7
Joel Seidel <joeldseidel@gmail.com>
2018-12-31 01:29:34 -0500

    Update controller docs

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -19,11 +19,16 @@ public class Controller : MonoBehaviour {
      */
     void initTowns()
     {
+        //Town colors array
         string[] colors = { "red", "blue", "green", "yellow", "black", "beige" };
+        //Create 6 towns
         for (int i = 0; i < 6; i++)
         {
+            //Define the empty game object to house the town monobehaviour script
             GameObject thisTownObj = Instantiate(new GameObject("town-name: " + colors[i]));
+            //Define the town mono object
             Town thisTown = thisTownObj.AddComponent<Town>();
+            //Initialize the town
             thisTown.init(colors[i]);
         }
     }
