46fddf5e5500e9abe064ce425f21e0af8fd75321
Joel Seidel <joeldseidel@gmail.com>
2018-12-31 00:25:08 -0500

    Create goods class

---
+++ Assets/Scripts/Good.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/**
+ * Author: Joel Seidel
+ * Good object
+ */
+namespace Assets.Scripts
+{
+    class Good
+    {
+        public string name;
+        public int id;
+        public double baseUtilityValue;
+    }
+}
